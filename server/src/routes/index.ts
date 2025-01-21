7e6f4d449a8b76b1e85d4e6244390ba115e48f3c
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2025-01-20 21:15:19 -0500

    updated .env file, index.ts file and package-lock.json files

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -13,6 +13,6 @@ router.use('/api', apiRoutes);
 // serve up react front-end in production
 router.use((_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
-});
+});z
 
 export default router;
